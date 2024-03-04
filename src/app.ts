724dc571f15581b661af738f3e5210e6c5867c91
ahmedkatan410 <ahmed.katan89@gmail.com>
2024-03-05 00:56:16 +0300

    start

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
 // Code goes here!
+
+class ProjectInput {
+     templateElemnt: HTMLTemplateElement;
+     constructor() {
+          this.templateElemnt = document.getElementById("project-input")!;
+          //   this.hostElement=
+     }
+}
