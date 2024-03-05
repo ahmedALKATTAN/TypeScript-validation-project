8b34ef541c6bae85a12f7e25a094064610d40c58
ahmedkatan410 <ahmed.katan89@gmail.com>
2024-03-05 19:39:59 +0300

    add list

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 // Code goes here!
 
 class ProjectInput {
-     templateElemnt: HTMLTemplateElement;
+     templateElement: HTMLTemplateElement;
+     hostElement: HTMLDivElement;
+     element: HTMLFormElement;
      constructor() {
-          this.templateElemnt = document.getElementById("project-input")!;
-          //   this.hostElement=
+          this.templateElement = document.getElementById(
+               "project-input"
+          )! as HTMLTemplateElement;
+          this.hostElement = document.getElementById("app")! as HTMLDivElement;
+          const importedNode = document.importNode(
+               this.templateElement.content,
+               true
+          );
+
+          this.element = importedNode.firstElementChild as HTMLFormElement;
+          this.attach();
+     }
+
+     private attach() {
+          this.hostElement.insertAdjacentElement("afterbegin", this.element);
      }
 }
+
+const porjectInput = new ProjectInput();
